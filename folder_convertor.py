49314f8c959ee1acb1166d2eec2a0d1fd4865d8d
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2022-07-18 12:06:20 +1000

    Update folder_convertor.py

--- folder_convertor.py
+++ folder_convertor.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals
 import youtube_dl
 import re
 
@@ -12,8 +11,8 @@ config = {
 }
 
 bookmarks = open("bookmark.txt",'r')
-BM_heading = bookmarks.readline()
-print ("Converting bookmark links for bookmark:", BM_heading) 
+BM_heading = bookmarks.readline().strip()
+print ("Converting bookmark links for bookmark folder:", BM_heading + '\n') 
 
 """
 Thank you to @brunodles for this youtube regex pattern
@@ -21,7 +20,8 @@ Can be found at: https://gist.github.com/brunodles/927fd8feaaccdbb9d02b
 """
 pattern = '(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?'
 
-count = 0
+count = 0 
+success = 0
 num_lines = bookmarks.readlines() 
 for line in num_lines:
     url = line.strip()
@@ -29,12 +29,16 @@ for line in num_lines:
     #If url is a youtube link 
     if re.match(pattern, url):
         count += 1
-        print("\nUrl {}: {}".format(count, url))
+        print("\nConverting url {}: {}".format(count, url))
         try:
             youtube_dl.YoutubeDL(config).download([url])
+            success += 1
         except:
             print("Download unsuccessful") 
     else:
-        print (url, "is not a youtube link. Skipped")
+        print(url, "is not a youtube link. Skipped.")
 
 bookmarks.close()
+
+print("\nConversion completed for folder:", BM_heading)
+print(int((success/count)*100),"%","of folder downloaded successfully.")
